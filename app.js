4e451854f2e796e97621ad6a794ac59620183407
Sivaraj P <sivarajp237@gmail.com>
2019-12-15 04:51:13 +0530

    Added swagger Configuration

--- app.js
+++ app.js
@@ -4,14 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var swaggerJSDoc = require('swagger-jsdoc');
 
 var winston = require('./utilities/Logger');
 var indexRouter = require('./routes/index');
 
 var ordersRouter = require("./routes/analysis/analysis");
+const swaggerDefinition = {
+  info: {
+    title: 'Aggregator service API',
+    version: '1.0.0',
+    description: 'Endpoints for Aggregator service',
+  },
+  host: 'localhost:3000',
+  basePath: '/api/analysis'
+};
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/index.js','./routes/analysis/analysis.js'],
+};
 
-
+const swaggerSpec = swaggerJSDoc(options);
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,8 +48,10 @@ app.use(bodyParser.urlencoded({
 
 
 app.use(bodyParser.json());
-
-
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 app.use('/', indexRouter);
 try{
 app.use('/api/analysis',ordersRouter);
--- routes/analysis/analysis.js
+++ routes/analysis/analysis.js
@@ -8,17 +8,86 @@ import ValidationService from './services/validationService';
 var router = express.Router();
 
 router.route('/fetchAllOrdersInCityPerDay')
+/**
+ * @swagger
+ * /fetchAllOrdersInCityPerDay:
+ *   post:
+ *     tags:
+ *       - Orders
+ *     description:  Returns  all Orders in a city on the given date
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of orders
+ *     parameters:
+ *       - name: city
+ *         description: Name of the city
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: date
+ *         description: Date
+ *         in: body
+ *         required: true
+ *         type: string
+ *         format : date-time
+ *         
+ */
     .post(ValidationService.validateFetchAllOrdersInCityPerDay,fetchAllOrdersInCityPerDay )
     .get(AnalysisService.getNotSupported)
     .put(AnalysisService.putNotSupported)
     .patch(AnalysisService.putNotSupported);
-  
+  /**
+ * @swagger
+ * /fetchAmountOfOrdersInCityPerDay:
+ *   post:
+ *     tags:
+ *       - Orders
+ *     description:  Returns total amount of all Orders in a city on the given date
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of orders
+ *     parameters:
+ *       - in: body
+ *         properties : 
+ *          city:
+ *           description: Name of the city
+ *           required: true
+ *           type: string
+ *          date :
+ *           description: Date
+ *           required: true
+ *           type: string
+ *           format : date-time
+ *         
+ */
     router.route('/fetchAmountOfOrdersInCityPerDay')
     .post(ValidationService.validateTotalAmountInCityPerDay,fetchAmountOfOrdersInCityPerDay )
     .get(AnalysisService.getNotSupported)
     .put(AnalysisService.putNotSupported)
     .patch(AnalysisService.patchNotSupported);
-
+/**
+ * @swagger
+ * /getRestaurantsByCity:
+ *   get:
+ *     tags:
+ *       - Orders
+ *     description: Returns total amount of all Orders in a city on the given date
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of orders
+ *     parameters:
+ *       - name: city
+ *         description: Name of the city
+ *         in: query
+ *         required: true
+ *         type: string
+ */       
     router.route('/getRestaurantsByCity')
     .get(getRestaurantsByCity)
     .post(AnalysisService.postNotSupported)
